bab659939619778734d0751d12a96a892e636205
nadavalali <nadav.alali@mail.huji.ac.il>
2022-07-30 14:03:09 +0300

    create saved data folder if not exists

--- components/datasets/utils.py
+++ components/datasets/utils.py
@@ -5,4 +5,7 @@ SAVED_DATASETS = "saved_datasets"
 
 def get_data_folder_path():
     dirname = os.path.dirname(__file__)
-    return os.path.join(dirname, SAVED_DATASETS)
+    saved_data_path = os.path.join(dirname, SAVED_DATASETS)
+    if not os.path.exists(saved_data_path):
+        os.makedirs(saved_data_path)
+    return saved_data_path
