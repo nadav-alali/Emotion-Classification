09b53039c439932a2e4cc126f1b5ae6691842ec1
nadavalali <nadav.alali@mail.huji.ac.il>
2022-07-30 18:42:14 +0300

    glove gets sentence and not a single word

--- components/text_handler/embedding/glove_embedding.py
+++ components/text_handler/embedding/glove_embedding.py
@@ -10,4 +10,4 @@ class GloveEmbedding(Embedding):
         self.model = GloVe(name='6B', dim=EMBEDDING_SIZE)
 
     def embed(self, sentence) -> list:
-        return [self.model.get_vecs_by_tokens(word) for word in sentence]
+        return None if len(sentence) == 0 else self.model.get_vecs_by_tokens(sentence)
