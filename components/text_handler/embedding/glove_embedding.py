added GRU model impl still need to fix scaling for loss, ROC and F1

components/text_handler/embedding/glove_embedding.py
@@ -1,8 +1,10 @@
+import torch
 from torchtext.vocab import GloVe
 
 from components.text_handler.embedding.embedding import Embedding
 
 EMBEDDING_SIZE = 100
+MAX_LENGTH = 32
 
 
 class GloveEmbedding(Embedding):
@@ -10,4 +12,7 @@ class GloveEmbedding(Embedding):
         self.model = GloVe(name='6B', dim=EMBEDDING_SIZE)
 
     def embed(self, sentence) -> list:
-        return None if len(sentence) == 0 else self.model.get_vecs_by_tokens(sentence)
+        embedded = self.model.get_vecs_by_tokens(sentence)
+        if embedded.shape[0] != EMBEDDING_SIZE or embedded.shape[1] != EMBEDDING_SIZE:
+            embedded = torch.nn.functional.pad(embedded, (0, 0, 0, MAX_LENGTH - embedded.shape[0]))
+        return torch.unsqueeze(embedded, 0)