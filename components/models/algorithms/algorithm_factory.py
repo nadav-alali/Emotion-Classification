d6db64035ad040525fb38cd24a3de986449ac8ac
nadavalali <nadav.alali@mail.huji.ac.il>
2022-08-16 21:51:03 +0300

    algorithm factory impl

--- components/models/algorithms/algorithm_factory.py
+++ components/models/algorithms/algorithm_factory.py
@@ -1,1 +1,10 @@
-# todo
+from components.models.algorithms.algorithms_enum import ClassifierAlgorithm
+from components.models.algorithms.gru import GRU
+from components.models.algorithms.lstm import LSTM
+
+
+def algorithms_factory(algorithm: ClassifierAlgorithm, input_size, output_size, hidden_size, device):
+    if algorithm == ClassifierAlgorithm.GRU:
+        return GRU(input_size, output_size, hidden_size).to(device)
+    elif algorithm == ClassifierAlgorithm.LSTM:
+        return LSTM(input_size, output_size, hidden_size).to(device)
